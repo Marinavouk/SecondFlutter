fede13bfe97415d4f6af9343b9e17e343d17addb
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2025-06-29 22:35:49 +0200

    Add samsung 22

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
-
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('hello flutter'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello everyone'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+      
+      },
+      child: Text('click'),
+    ),
+  ),
+));
